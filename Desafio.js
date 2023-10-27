320ee0b34022b28cbed31b563a654def4e8ad08f
Jean <146673894+JajaPenfore@users.noreply.github.com>
2023-10-27 18:05:27 -0300

    Desafio.js

--- Desafio.js
+++ Desafio.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+let NomeDoHeroi = "Guts"
+let XP = 100000
+    if (XP <=1000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Ferro")
+}   else if (XP <=2000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Bronze")
+}   else if (XP <=5000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Prata")
+}   else if (XP <= 7000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Ouro")
+}   else if (XP <= 8000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Platina")
+}   else if (XP <= 9000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Ascendente")
+}   else if (XP <= 10000){
+    console.log("O Herói de nome " + NomeDoHeroi + "está no nível de " + "Imortal")
+}   else {
+    console.log("O Herói de nome " + NomeDoHeroi + " está no nível de " + "Radiante")
+}   
